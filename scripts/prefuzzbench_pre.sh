a83cf8d47ce30c1e57bcdf6e34d1bc97741c9ee5
fang0216 <158567686+fang0216@users.noreply.github.com>
2024-11-26 14:54:11 +0800

    Update prefuzzbench_pre.sh

--- scripts/prefuzzbench_pre.sh
+++ scripts/prefuzzbench_pre.sh
@@ -9,13 +9,13 @@ WORKDIR="/root"
 
 # create one container for pre
 # $PFBENCH: /root/prefuzzbench
-pid=$(docker run -v $PFBENCH/pcaps/:/opt/pcaps/ -itd ${PRE} /bin/bash -c "python /root/NetPlier/netplier/main.py -i /opt/pcaps/${PROTOCOL}.pcap -o zwl_result/MODBUS_1out -r /opt/pits/${PROTOCOL}.out -l 5")
+pid=$(docker run -v $PFBENCH/pcaps/:/opt/ -itd ${PRE} /bin/bash -c "python /root/NetPlier/netplier/main.py -i /opt/${PROTOCOL}.pcap -o zwl_result/MODBUS_1out -r /opt/${PROTOCOL}.out -l 5")
 
 docker wait ${pid} > /dev/null
 
 #collect the pre result from the container
 printf "\n${PRE^^}: Collecting results from container ${pid} and save them to ${PFBENCH}/in"
-docker cp ${pid}:$/opt/pits/${PROTOCOL}.out $PFBENCH/in/
+sudo docker cp ${pid}:/opt/${PROTOCOL}.out $PFBENCH/in/
 
 # 等待所有后台进程结束
 wait
