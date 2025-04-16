aaa8cbd6a5d7bb270b87f5b03c550bd99cb6a3e3
fang0216 <158567686+fang0216@users.noreply.github.com>
2025-04-16 16:53:18 +0800

    Update PRE2Fuzz_pre.sh

--- scripts/PRE2Fuzz_pre.sh
+++ scripts/PRE2Fuzz_pre.sh
@@ -1,29 +1,54 @@
 #!/bin/bash
-PROTOCOL=$1   #name of the protocol
-PRE=$2        #pre tool name
-TIMEOUT=$3    #time for fuzzing
-DELETE=$4
-# prefuzzbench_pre.sh lightftp netplier:out 300
+
+INPUT_FILE=""
+TOOL=""
+OUTPUT_DIR=""
+
+while getopts "i:r:o:" opt; do
+  case $opt in
+    i)
+      INPUT_FILE=$OPTARG
+      ;;
+    r)
+      TOOL=$OPTARG
+      ;;
+    o)
+      OUTPUT_DIR=$OPTARG
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+if [ -z "$INPUT_FILE" ] || [ -z "$TOOL" ] || [ -z "$OUTPUT_DIR" ]; then
+  echo "Usage: $0 -i <input_file> -r <tool> -o <output_dir>"
+  exit 1
+fi
+
+PROTOCOL=$(basename "$INPUT_FILE" .pcap)
 
 WORKDIR="/root"
 
-# create one container for pre
-# $PFBENCH: /root/prefuzzbench
-pid=$(docker run -v $PFBENCH/pcaps/:/opt/ -itd ${PRE} /bin/bash -c "python /root/NetPlier/netplier/main.py -i /opt/${PROTOCOL}.pcap -o zwl_result/MODBUS_1out -r /opt/${PROTOCOL}.out -l 5")
+PFBENCH="/root/pre2fuzz"
+pid=$(docker run -v "$PFBENCH/pcaps:/opt/" -itd "$TOOL" /bin/bash -c "python /root/NetPlier/netplier/main.py -i /opt/$INPUT_FILE -o zwl_result/MODBUS_1out -r /opt/$PROTOCOL.out -l 5")
 
-docker wait ${pid} > /dev/null
+docker wait "$pid" > /dev/null
 
-#collect the pre result from the container
-printf "\n${PRE^^}: Collecting results from container ${pid} and save them to ${PFBENCH}/in"
-sudo docker cp ${pid}:/opt/${PROTOCOL}.out $PFBENCH/in/
+printf "\n${TOOL^^}: Collecting results from container$pid and save them to $OUTPUT_DIR"
+sudo docker cp "$pid:/opt/$PROTOCOL.out" "$OUTPUT_DIR/"
 
-# 等待所有后台进程结束
 wait
 
-if [ ! -z $DELETE ]; then
-printf "\nDeleting ${pid}"
-docker stop ${pid}
-docker rm ${pid} # Remove container now that we don't need it
+if [ ! -z "$DELETE" ]; then
+  printf "\nDeleting $pid"
+  docker stop "$pid"
+  docker rm "$pid"
 fi
 
-printf "\n${PRE^^}: I am done!\n"
+printf "\n${TOOL^^}: I am done!\n"
